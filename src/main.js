e325bf020ac89afaf8435cecfc77cd95b157a347
Vladislav <mainrub06@gmail.com>
2019-09-21 14:59:53 +0300

    module2-task1

--- src/main.js
+++ src/main.js
@@ -1,3 +1,25 @@
-const hallo = `hallo world!`;
+import search from "./components/search.js";
+import account from "./components/account.js";
+import menu from "./components/menu.js";
+import sort from "./components/sort.js";
+import films from "./components/films.js";
 
-console.log(hallo);
+const doc = document;
+const $header = doc.querySelector(`.header`);
+const $main = doc.querySelector(`.main`);
+
+const renderHTML = function (inner, target) {
+  const element = document.createElement(`template`);
+  element.innerHTML = inner();
+  target.appendChild(element.content);
+};
+
+const renderComponents = () => {
+  renderHTML(search, $header);
+  renderHTML(account, $header);
+  renderHTML(menu, $main);
+  renderHTML(sort, $main);
+  renderHTML(films, $main);
+};
+
+renderComponents();
